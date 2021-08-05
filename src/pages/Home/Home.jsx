f980edddca416369aba349f28a865532ecf27e37
uroge <67282212+uroge@users.noreply.github.com>
2021-08-05 09:16:54 +0200

    Fetch data and log it to the console

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,7 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import './Home.scss';
 
+import axios from '../../axios/axios';
+
 const Home = () => {
+    useEffect(() => {
+        axios.get('data.json')
+        .then(response => console.log(response.data.results))
+        .catch(error => console.log(error));
+    }, []);
+    
     return(
         <div className="home">
             Home
